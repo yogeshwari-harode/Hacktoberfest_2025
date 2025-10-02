e06b5414e89020149f8d5bb3b06fbdc48d3e7a41
Jayshri2003 <142375836+Jayshri2003@users.noreply.github.com>
2025-10-02 22:38:42 +0530

    Create rev.py

---
+++ rev.py
@@ -0,0 +1,11 @@
+# Reverse a string in Python
+
+# Input string
+text = "Hello, world!"
+
+# Reversing the string
+reversed_text = text[::-1]
+
+# Output
+print("Original string:", text)
+print("Reversed string:", reversed_text)
